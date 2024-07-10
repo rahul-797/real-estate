093accc0f2a469a81ea29aa63850fe09bb46cfe1
rahul-797 <rahulsangwan797@gmail.com>
2024-07-10 16:07:09 +0530

    removed 3D model

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Content from "./components/Content";
 import Header from "./components/Header";
-import Scene from "./components/House";
 
 export default function App() {
   return (
     <ChakraProvider>
       <Header />
       <Content />
-      <Scene />
     </ChakraProvider>
   );
 }
